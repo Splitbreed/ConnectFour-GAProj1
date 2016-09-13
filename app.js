a612e8a5457edd84ef24fa9442d3f51cec193eb2
Adam Youngblood <ayoungbl0d@gmail.com>
2016-09-13 12:08:03 -0500

    Building app logic with on click
    Built basic on click and onload to build board and on column click
    play a piece. each piece is dictated by the currentPlayer var, with
    true making a piece white, and false making a piece black.

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
+var currentPlayer = true;
 var App = {
-
+  dropPiece: function() {
+    var thisRows = $(this).find('.row');
+    for (var k = 6; k >= 0; k--) {
+      var isBlack = $(thisRows[k]).hasClass('black');
+      var isWhite = $(thisRows[k]).hasClass('white')
+      if (isBlack === true || isWhite === true) {
+        alert('Cannot be played');
+        break;
+      } else {
+        if (currentPlayer === true) {
+          $(thisRows[k]).addClass('white');
+          currentPlayer = !currentPlayer;
+        } else if (currentPlayer === false){
+          $(thisRows[k]).addClass('black');
+          currentPlayer = !currentPlayer;
+          return;
+        }
+      }
+    }
+  } //end dropPiece
 }
 var UI = {
   createColumns: function() {
     for (var i = 0; i < 7; i++) {
       var columns = $('<div>');
       columns.addClass('column');
+      if(i > 0 && i < 6){
+        columns.addClass('centralColumn')
+      }
       for (var j = 0; j < 6; j++) {
         var rows = $('<div>');
         rows.addClass('row');
         if (j > 0 && j < 5) {
-          rows.attr('id', 'central');
+          rows.addClass('centralRow');
         }
         columns.append(rows);
       }
@@ -21,5 +44,6 @@ var UI = {
 
 
 window.onload = function() {
-
+  UI.createColumns();
+  $('.column').on('click', App.dropPiece)
 }
