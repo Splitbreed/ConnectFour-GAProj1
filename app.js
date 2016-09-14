bb7bd6e38be6815ec9a6384d78e2d81d9bcf4e9c
Adam Youngblood <ayoungbl0d@gmail.com>
2016-09-13 19:47:51 -0500

    Added turn notice and coloring to css and app
    Finished basic game logic, can be played with two humans. Still
    working on win conditions.

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 var currentPlayer = true;
+var playable = [];
 var App = {
-  dropPiece: function() {
-    var thisRows = $(this).find('.row');
-    for (var k = 6; k >= 0; k--) {
-      var isBlack = $(thisRows[k]).hasClass('black');
-      var isWhite = $(thisRows[k]).hasClass('white')
+  dropPieceWhite: function() {
+    $(playable[0]).addClass('white');
+    currentPlayer = !currentPlayer;
+    UI.changeTurn();
+  }, //end dropPiece
+  dropPieceBlack: function() {
+    $(playable[0]).addClass('black');
+    currentPlayer = !currentPlayer;
+    UI.changeTurn();
+  }, //end dropPieceBlack
+  isPlayable: function(whichCol) {
+    playable = [];
+    for (var k = 5; k >= 0; k--) {
+      // console.log(playable);
+      // console.log(whichCol);
+      var isBlack = $(whichCol[k]).hasClass('black');
+      // console.log(isBlack);
+      var isWhite = $(whichCol[k]).hasClass('white')
+      // console.log(isWhite);
       if (isBlack === true || isWhite === true) {
-        alert('Cannot be played');
-        break;
-      } else {
-        if (currentPlayer === true) {
-          $(thisRows[k]).addClass('white');
-          currentPlayer = !currentPlayer;
-        } else if (currentPlayer === false){
-          $(thisRows[k]).addClass('black');
-          currentPlayer = !currentPlayer;
+        if (playable.length === 0 && k === 0){
+          alert("That column is full, and cannot be played");
         }
-      }
-    }
-  } //end dropPiece
-}
+      } else {
+        playable.push(whichCol[k]);
+      } //end else
+    } //end for
+    return playable;
+  } //end isPlayable
+} //end App
 var UI = {
   createColumns: function() {
     for (var i = 0; i < 7; i++) {
@@ -41,12 +52,34 @@ var UI = {
   }, //endCreateColumns
   clearBoard: function() {
     $('.row').removeClass('black white');
+  }, //end clearBoard
+  playGame: function() {
+    var thisRows = $(this).find('.row');
+    App.isPlayable(thisRows);
+    // console.log(playable);
+    // console.log(playable.length);
+    if (playable.length > 0 && currentPlayer === true) {
+      App.dropPieceWhite();
+    } else if (playable.length > 0 && currentPlayer === false) {
+      App.dropPieceBlack();
+    } //end else if
+  }, //end playGame
+  changeTurn: function() {
+    if (currentPlayer === true) {
+      $('#turn').text('White');
+      $('#turn').removeClass('turnBlack');
+      $('#turn').addClass('turnWhite');
+    } else if (currentPlayer ===false) {
+      $('#turn').text('Black');
+      $('#turn').removeClass('turnWhite');
+      $('#turn').addClass('turnBlack');
+    }
   }
 } //endUI
 
 
 window.onload = function() {
   UI.createColumns();
-  $('.column').on('click', App.dropPiece);
+  $('.column').on('click', UI.playGame);
   $('#reset').on('click', UI.clearBoard);
 }
